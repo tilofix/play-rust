7d96979d9bd69d1d75180f714533dc23a3ec1941
Tilo Wirkner <tilo@tilofix.de>
2021-01-18 09:19:30 +0100

    Prepared to debug macros with pretty expansion.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+// option '--pretty' requires rustc's unstable options which requires 'nightly'.
+// -*- compile-command: "cargo +nightly rustc -- -Z unstable-options --pretty expanded" -*-
+
 fn main() -> () {
-    let x = 5 + 5;
-    println!("Hello, world! x is {}", x);
+    let x: i32 = 5;
+    let _v: Vec<u64> = vec![1,2,3];
+    let s: &str = "Tilo";
+    println!("Hello, world! x is {}; s is {}", x, s);
 }
