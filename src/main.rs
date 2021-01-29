80538043848c14cc55b56b967fbd2ebeae218cd0
Tilo Wirkner <tilo@tilofix.de>
2021-01-29 22:05:21 +0100

    Work with 'racer' to find implemented traits

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
-// option '--pretty' requires rustc's unstable options which requires 'nightly'.
 // -*- compile-command: "cargo +nightly rustc -- -Z unstable-options --pretty expanded" -*-
+// option '--pretty' requires rustc's unstable options which requires 'nightly'.
+// use M-x 'compile' to get expanded version of the source code
+// use M-x 'rust-compile' to actually compile the source code
 
 fn main() -> () {
     let x: i32 = 5;
-    let _v: Vec<u64> = vec![1,2,3];
+    let c2: char = '❤'; // UTF-8
     let s: &str = "Tilo";
-    println!("Hello, world! x is {}; s is {}", x, s);
+    println!("Hello, world! x is {} with a {}; s is {}", x, c2, s);
+    
+    let v = vec![1,2,3]; // The vec! macro for convenient initialization
+    let sl: &[isize] = &v; // A Vec can be mutable. Slices are read-only objects. 
+    let sl_cat: &[isize] = &[sl, &[4,5,6]].concat();
+    for elem in sl_cat { println!("{}", elem); }
 }
